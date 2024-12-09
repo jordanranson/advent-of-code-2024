4c28130535fd070ca80090de50ab006dc7ff56f6
Jordan Ranson <jordan@jordanranson.com>
2024-12-08 23:50:44 -0700

    empty days

--- src/solutions.rs
+++ src/solutions.rs
@@ -23,12 +23,18 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
+mod day8;
+mod day9;
 
-pub const SOLUTIONS: [Solution; 6]= [
+pub const SOLUTIONS: [Solution; 9]= [
     day1::SOLUTION,
     day2::SOLUTION,
     day3::SOLUTION,
     day4::SOLUTION,
     day5::SOLUTION,
     day6::SOLUTION,
+    day7::SOLUTION,
+    day8::SOLUTION,
+    day9::SOLUTION,
 ];
